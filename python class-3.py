50fd09be4c3ab810f3cac50cd88f595adbd03bde
Noman731 <mdnomanislam753@gmail.com>
2025-05-12 23:49:07 +0600

    Add files via upload

---
+++ python class-3.py
@@ -0,0 +1,37 @@
+ #arithmetic operator
+a=5
+b=10
+c=a+b
+d=5
+e= int(c/d)
+f=a*b
+g=10+6j
+h= a+g
+print("c=",c)
+print("e=",e)
+print("f=",f)
+print("h=",h)
+# comparison operator
+a=4
+b=7
+print(a>b)
+print(a<b)
+name=input("inter your name:")
+age=input("inter your age:")
+print("your name is", name)
+print("your age is=", age)
+
+ideal_markS=80
+marks=50
+if marks < ideal_markS:
+     print("fail")
+else:
+     print("pass")
+ideal_marks=80
+marks=int(input("Enter your marks:"))
+if marks < ideal_marks:
+     print("Fail")
+elif marks==ideal_marks:
+     print("pass")
+else:
+     print("Excelent")
